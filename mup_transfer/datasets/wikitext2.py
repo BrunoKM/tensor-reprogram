0f2b43a0128fba48c6c94fdea4911a7200616a22
Bruno Mlodozeniec <bkmlodozeniec@gmail.com>
2023-03-11 20:15:19 +0000

    Formatting

--- mup_transfer/datasets/wikitext2.py
+++ mup_transfer/datasets/wikitext2.py
@@ -16,15 +16,17 @@ def wikitext_constructor(
     test_batch_size: int,
     bptt: int,
 ) -> tuple[LMDataLoader, dict[str, LMDataLoader]]:
-    train_iter = WikiText2(str(root), split='train')
+    train_iter = WikiText2(str(root), split="train")
     tokenizer = get_tokenizer(None)
-    vocab = build_vocab_from_iterator(map(tokenizer, train_iter), specials=['<unk>', '<eos>'])
-    vocab.set_default_index(vocab['<unk>'])
-    # print(len(vocab))
+    vocab = build_vocab_from_iterator(map(tokenizer, train_iter), specials=["<unk>", "<eos>"])
+    vocab.set_default_index(vocab["<unk>"])
 
     def data_process(raw_text_iter: torch.utils.data.IterableDataset) -> torch.LongTensor:
         """Converts raw text into a flat Tensor."""
-        data = [torch.tensor(vocab(tokenizer(item) + ['<eos>']), dtype=torch.long) for item in raw_text_iter]
+        data = [
+            torch.tensor(vocab(tokenizer(item) + ["<eos>"]), dtype=torch.long)
+            for item in raw_text_iter
+        ]
         return torch.cat(tuple(filter(lambda t: t.numel() > 0, data)))
 
     train_iter, val_iter, test_iter = WikiText2()
@@ -35,12 +37,14 @@ def wikitext_constructor(
     train_dataloader = LMDataLoader([train_data], bs=train_batch_size, seq_len=bptt)
     valid_dataloader = LMDataLoader([val_data], bs=test_batch_size, seq_len=bptt)
     test_dataloader = LMDataLoader([test_data], bs=test_batch_size, seq_len=bptt)
-    return train_dataloader, {'valid': valid_dataloader, "test": test_dataloader}
+    return train_dataloader, {"valid": valid_dataloader, "test": test_dataloader}
 
 
-if __name__=='__main__':
-    train, eval_dict = wikitext_constructor(Path(__file__).parent.parent / "data", 2, 10)
-    for ii, data in enumerate(eval_dict['test']):
+if __name__ == "__main__":
+    train, eval_dict = wikitext_constructor(
+        root=Path(__file__).parent.parent / "data", train_batch_size=2, test_batch_size=3, bptt=10
+    )
+    for ii, data in enumerate(eval_dict["test"]):
         if ii == 3:
             break
         print(data[0])
