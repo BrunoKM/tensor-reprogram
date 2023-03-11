6f0edd70ecf6c602cdfc9ef68e49427197466969
runame <runa.eschenhagen@student.uni-tuebingen.de>
2023-03-11 23:49:56 +0100

    Add wikitext_transformer slurm job script

---
+++ run_wikitext_transformer.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH -p gpu-2080ti                       # partition
+#SBATCH --gres=gpu:rtx2080ti:1              # type and number of gpus
+#SBATCH --time=72:00:00                     # job will be cancelled after max. 72h
+#SBATCH --output=wikitext_transformer_%A_%a.out
+#SBATCH --array=10-20
+
+# print info about current job
+scontrol show job $SLURM_JOB_ID
+
+export WANDB_API_KEY=9a337dffa0b41297c8870d83df890ea39dc02163
+
+# Assuming you have activated your conda environment
+for width in 64 128 256 512 1024 2048 4096; do
+    python scripts/run.py \
+        +experiment=wikitext_transformer \
+        optimization.lr=1e-$SLURM_ARRAY_TASK_ID \
+        transformer_config.d_model=$width
+done 
