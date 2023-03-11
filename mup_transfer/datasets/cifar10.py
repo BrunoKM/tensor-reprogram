50d795c8d295a308b545c4e785587988d8e7628d
runame <runa.eschenhagen@student.uni-tuebingen.de>
2023-03-11 13:22:17 +0000

    Fix cifar10_constructor

--- mup_transfer/datasets/cifar10.py
+++ mup_transfer/datasets/cifar10.py
@@ -1,18 +1,21 @@
 from os import PathLike
+from typing import Optional, Sequence
+
 from torch.utils.data import Dataset, Subset
 from torchvision.datasets import CIFAR10
-from torchvision.transforms import ToTensor
-
-
-from typing import Optional, Sequence
+from torchvision import transforms
 
 
 def cifar10_constructor(
     root: PathLike,
     subset_idxs: Optional[Sequence[int]] = None,
 ) -> tuple[Dataset, dict[str, Dataset]]:
-    train_set = CIFAR10(root=str(root), train=True, download=True, transform=ToTensor())
-    test_set = CIFAR10(root=str(root), train=True, download=True, transform=ToTensor())
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+    ])
+    train_set = CIFAR10(root=str(root), train=True, download=True, transform=transform)
+    test_set = CIFAR10(root=str(root), train=False, download=True, transform=transform)
     if subset_idxs is not None:
         train_set = Subset(train_set, subset_idxs)
         test_set = Subset(test_set, subset_idxs)
