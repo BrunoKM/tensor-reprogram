457b6dbf07668f46cef4317037d49e09b114f90e
runame <runa.eschenhagen@student.uni-tuebingen.de>
2023-03-21 16:18:56 +0000

    Fix path to run script

--- scripts/launch_run.py
+++ scripts/launch_run.py
@@ -11,7 +11,7 @@ def main(file_path):
     init_scales_per_param = python_dict["initialisation"]["init_scales_per_param"]
     config_string = (f"++optimization.per_param_lr='{per_param_lr}' "
                      + f"++initialisation.init_scales_per_param='{init_scales_per_param}'")
-    cmd_string = f"python3 ./run.py +experiment=cifar10_mlp mlp_config.hidden_sizes=\[256,256\] {config_string}"
+    cmd_string = f"python3 scripts/run.py +experiment=cifar10_mlp mlp_config.hidden_sizes=\[256,256\] {config_string}"
     os.system(cmd_string)
 
 
