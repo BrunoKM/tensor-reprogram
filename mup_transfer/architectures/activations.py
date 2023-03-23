f5ce80157a080c5f96e52c9d8380138f2f1d463f
Bruno Mlodozeniec <bkmlodozeniec@gmail.com>
2023-03-23 16:45:08 +0000

    Add sigma gelu

--- mup_transfer/architectures/activations.py
+++ mup_transfer/architectures/activations.py
@@ -1,7 +1,10 @@
 from enum import Enum
 from functools import partial
+import math
 from typing import Optional
+import torch
 import torch.nn as nn
+from torch import Tensor
 
 
 class ActivationType(Enum):
@@ -9,6 +12,7 @@ class ActivationType(Enum):
     TANH = "tanh"
     SILU = "silu"
     GELU = "gelu"
+    SIGMA_GELU = "sigma_gelu"
 
 
 def get_activation(activation_type: ActivationType, **activation_kwargs):
@@ -20,5 +24,29 @@ def get_activation(activation_type: ActivationType, **activation_kwargs):
         return partial(nn.SiLU, **activation_kwargs)
     elif activation_type == ActivationType.GELU:
         return partial(nn.GELU, **activation_kwargs)
+    elif activation_type == ActivationType.SIGMA_GELU:
+        return partial(SigmaGeLU, **activation_kwargs)
     else:
         raise ValueError(f"Unknown activation type: {activation_type}")
+
+
+class SigmaGeLU(nn.Module):
+    def __init__(self, sigma: float = 1.0, approximate: bool = True):
+        super().__init__()
+        self.sigma = sigma
+        self.approximate = "tanh" if approximate else "none"
+
+    def forward(self, x: Tensor) -> Tensor:
+        # return (
+        #     0.5 * self.sigma * nn.functional.gelu(x / self.sigma, approximate=self.approximate)
+        #     + 0.5 * x
+        #     + (self.sigma / (2 * math.sqrt(math.pi))) * torch.exp(-(x / self.sigma) ** 2)
+        # )
+        return (
+            0.5 * x * torch.erf(x / self.sigma)
+            + 0.5 * x
+            + (self.sigma / (2 * math.sqrt(math.pi))) * torch.exp(-(x / self.sigma) ** 2)
+        )
+
+    def extra_repr(self):
+        return f"sigma={self.sigma}"
