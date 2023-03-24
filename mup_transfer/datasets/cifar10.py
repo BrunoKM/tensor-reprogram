6505c54ef470e4b901d7efe4ede229e4892a8da0
runame <runa.eschenhagen@student.uni-tuebingen.de>
2023-03-24 18:27:43 +0100

    Add data augmentation option for cifar10

--- mup_transfer/datasets/cifar10.py
+++ mup_transfer/datasets/cifar10.py
@@ -8,14 +8,23 @@ from torchvision import transforms
 
 def cifar10_constructor(
     root: PathLike,
+    use_data_augmentation: bool = False,
     subset_idxs: Optional[Sequence[int]] = None,
 ) -> tuple[Dataset, dict[str, Dataset]]:
-    transform = transforms.Compose([
+    base_transforms = [
         transforms.ToTensor(),
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-    ])
-    train_set = CIFAR10(root=str(root), train=True, download=True, transform=transform)
-    test_set = CIFAR10(root=str(root), train=False, download=True, transform=transform)
+    ]
+    test_transforms = transforms.Compose(base_transforms)
+    train_transforms = transforms.Compose(
+        [
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomCrop(32, padding=4),
+        ]
+        + base_transforms
+    ) if use_data_augmentation else test_transforms
+    train_set = CIFAR10(root=str(root), train=True, download=True, transform=train_transforms)
+    test_set = CIFAR10(root=str(root), train=False, download=True, transform=test_transforms)
     if subset_idxs is not None:
         train_set = Subset(train_set, subset_idxs)
         test_set = Subset(test_set, subset_idxs)
