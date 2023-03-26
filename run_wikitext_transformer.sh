100bfa36a84534ac731c43084c297bca7ce0f0c5
runame <runa.eschenhagen@student.uni-tuebingen.de>
2023-03-26 14:05:27 +0200

    Update transformer slurm job script

--- run_wikitext_transformer.sh
+++ run_wikitext_transformer.sh
@@ -3,7 +3,7 @@
 #SBATCH --gres=gpu:rtx2080ti:1              # type and number of gpus
 #SBATCH --time=72:00:00                     # job will be cancelled after max. 72h
 #SBATCH --output=wikitext_transformer_%A_%a.out
-#SBATCH --array=10-20
+#SBATCH --array=4-16
 
 # print info about current job
 scontrol show job $SLURM_JOB_ID
@@ -14,6 +14,6 @@ export WANDB_API_KEY=9a337dffa0b41297c8870d83df890ea39dc02163
 for width in 64 128 256 512 1024 2048 4096; do
     python scripts/run.py \
         +experiment=wikitext_transformer \
-        optimization.lr=$(perl -e "print 2**-$SLURM_ARRAY_TASK_ID") \
+        optimization.default_lr=$(perl -e "print 2**-$SLURM_ARRAY_TASK_ID") \
         transformer_config.d_model=$width
 done 
